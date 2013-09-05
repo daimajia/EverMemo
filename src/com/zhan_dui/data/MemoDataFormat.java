b4e7193d9ac0db6441a4b9d0bb7a9ba49896369b
daimajia <xuanqinanhai@gmail.com>
2013-09-06 07:48:46 +0800

    初始化项目

---
+++ src/com/zhan_dui/data/MemoDataFormat.java
@@ -0,0 +1,11 @@
+package com.zhan_dui.data;
+
+
+public class MemoDataFormat {
+	
+	private long mCreateTime;
+	
+	public MemoDataFormat(){
+		
+	}
+}
---
+++ src/com/zhan_dui/evermemo/StartActivity.java
@@ -0,0 +1,23 @@
+package com.zhan_dui.evermemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+    
+}
